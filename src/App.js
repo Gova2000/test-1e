6d7dced2e4848ace5939eb2ac6ac7b4ac9fc8490
Gova2000 <138764817+Gova2000@users.noreply.github.com>
2023-11-13 13:49:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
+/* eslint-disable import/no-named-as-default-member */
+import {Component} from 'react'
 import {Route, Switch, Redirect} from 'react-router-dom'
 import Home from './components/Home/home'
 import Login from './components/login/login'
 
 import NotFound from './components/Notfound/notfound'
-
+import CartContext from './JJ'
 import './App.css'
 
 // These are the lists used in the application. You can move them to any component needed.
 
 // Replace your code here
-const App = () => (
-  <Switch>
-    <Route exact path="/" component={Home} />
-    <Route exact path="/register" component={Login} />
+class App extends Component {
+  state = {input: ''}
 
-    <Route path="/not-found" component={NotFound} />
-    <Redirect to="/not-found" />
-  </Switch>
-)
+  Add = put => {
+    this.setState({input: put})
+  }
+
+  render() {
+    const {input} = this.state
+
+    return (
+      <CartContext.Provider value={{input, Add: this.Add}}>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/register" component={Login} />
+
+          <Route path="/not-found" component={NotFound} />
+          <Redirect to="/not-found" />
+        </Switch>
+      </CartContext.Provider>
+    )
+  }
+}
 
 export default App
-
-/* <Route path="/" component={Register} />
-    <Route exact path="/" component={Home} /> */
