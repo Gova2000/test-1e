a2b60c7bc9876cf143262a0dc09e395483d32934
Gova2000 <138764817+Gova2000@users.noreply.github.com>
2023-11-13 13:48:42 +0530

    Update login.js

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,7 +1,9 @@
+/* eslint-disable import/no-named-as-default-member */
+
 import {Component} from 'react'
 import {withRouter} from 'react-router-dom'
 import Header from '../header/head'
-import Context from '../../JJ'
+import CartContext from '../../JJ'
 import './login.css'
 
 const topicsList = [
@@ -35,79 +37,88 @@ class Login extends Component {
     const {input} = this.state
     const {history} = this.props
 
-    if (input === '') {
-      this.setState({tog: true})
-    } else {
+    if (input !== '') {
       this.setState({tog: false})
       history.replace('/')
+    } else {
+      this.setState({tog: true})
     }
   }
 
-  input1 = e => {
-    this.setState({input: e.target.value})
-  }
+  
 
   render() {
     const {tog, input, option} = this.state
-    console.log(input)
 
-    return (
-      <Context.Provider
-        value={{
-          input,
-        }}
-      >
-        <div className="main">
-          <Header />
-          <div className="row">
-            <div className="imgdiv">
-              <img
-                src="https://assets.ccbp.in/frontend/react-js/meetup/website-register-img.png"
-                alt="website register"
-              />
-            </div>
+    
+      <CartContext.Consumer>
+  {value =>{
 
-            <form onSubmit={this.get}>
-              <h1>Let us join</h1>
-              <div className="margin">
-                <label htmlFor="input">NAME</label>
-                <div className="input">
-                  <input
-                    type="text"
-                    placeholder="Your name"
-                    id="input"
-                    onChange={this.input1}
+    const { Add, input } = value;
+    
+
+          const input1 = e => {
+         const newInput = e.target.value;
+            this.setState({ input: newInput });
+            Add(newInput);
+                }
+
+          return (<div className="main">
+              <Header />
+              <div className="row">
+                <div className="imgdiv">
+                  <img
+                    src="https://assets.ccbp.in/frontend/react-js/meetup/website-register-img.png"
+                    alt="website register"
                   />
                 </div>
+
+                <form onSubmit={this.get}>
+                  <h1>Let us join</h1>
+                  <div className="margin">
+                    <label htmlFor="input">NAME</label>
+                    <div className="input">
+                      <input
+                        type="text"
+                        placeholder="Your name"
+                        id="input"
+                        onChange={input1}
+                      />
+                    </div>
+                  </div>
+                  <div className="margin">
+                    <label htmlFor="select">TOPICS</label>
+                    <div className="input">
+                      <select
+                        id="select"
+                        value={option}
+                        onChange={e => {
+                          this.setState({option: e.target.value})
+                        }}
+                      >
+                        {topicsList.map(each => (
+                          <option key={each.id} value={each.id}>
+                            {each.displayText}
+                          </option>
+                        ))}
+                      </select>
+                    </div>
+                  </div>
+                  <button type="submit" onClick={this.get}>
+                    Register Now
+                  </button>
+                  {tog && <p className="p">Please enter your name</p>}
+                </form>
               </div>
-              <div className="margin">
-                <label htmlFor="select">TOPICS</label>
-                <div className="input">
-                  <select
-                    id="select"
-                    value={option}
-                    onChange={e => {
-                      this.setState({option: e.target.value})
-                    }}
-                  >
-                    {topicsList.map(each => (
-                      <option key={each.id} value={each.id}>
-                        {each.displayText}
-                      </option>
-                    ))}
-                  </select>
-                </div>
-              </div>
-              <button type="submit" onClick={this.get}>
-                Register Now
-              </button>
-              {tog && <p className="p">Please enter your name</p>}
-            </form>
-          </div>
-        </div>
-      </Context.Provider>
-    )
+            </div>
+          )
+
+          }
+          }
+        
+      </CartContext.Consumer>
+    
   }
 }
 
-export default withRouter(Login)
+export default Login
