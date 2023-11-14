6148df73ded06176b1bb9900ccb9e6ac6f77cedc
Gova2000 <138764817+Gova2000@users.noreply.github.com>
2023-11-14 13:11:27 +0530

    Update home.js

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -8,6 +8,42 @@ const Home = () => (
     {value => {
       const {input} = value
       console.log(input)
+import Header from '../header/head'
+import Register from '../registred'
+import CartContext from '../../JJ'
+import './home.css'
+
+const Home = () => (
+  <CartContext.Consumer>
+    {value => {
+      const {input, optionName} = value
+
+      return (
+        <>
+          {input !== '' ? (
+            <div>
+              <Header />
+
+              <div className="homebg">
+                <h1 className="h1">Hello {input}</h1>
+                <p>Welcome to {optionName}</p>
+                <img
+                  src="https://assets.ccbp.in/frontend/react-js/meetup/meetup-img.png"
+                  alt="meetup"
+                />
+              </div>
+            </div>
+          ) : (
+            <>
+              <Register />
+            </>
+          )}
+        </>
+      )
+    }}
+  </CartContext.Consumer>
+)
+export default Home
 
       return (
         <>
